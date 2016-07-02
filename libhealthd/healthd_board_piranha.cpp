8f351e45e52c0cec46959120035343d67d10087b
focuschou <focuschou@qq.com>
2016-07-02 21:50:17 +0800

    superior: update libhealthd

--- libhealthd/healthd_board_piranha.cpp
+++ libhealthd/healthd_board_piranha.cpp
@@ -30,9 +30,28 @@ void healthd_board_init(struct healthd_config *config)
     // others use defaults
 }
 
-
 int healthd_board_battery_update(struct android::BatteryProperties *props)
 {
     // return 0 to log periodic polled battery status to kernel log
     return 0;
+}
+
+void healthd_board_mode_charger_draw_battery(struct android::BatteryProperties*)
+{
+
+}
+
+void healthd_board_mode_charger_battery_update(struct android::BatteryProperties*)
+{
+
+}
+
+void healthd_board_mode_charger_set_backlight(bool)
+{
+
+}
+
+void healthd_board_mode_charger_init()
+{
+
 }
